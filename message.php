1f7e6f5cb17605aebf3f803eb7e7232853f2f3b8
Monty <monty@macbook.home>
2015-12-08 11:36:53 -0800

    PHP file displays content from variable.

---
+++ message.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<?php
+		$message= "Content";
+	?>
+	<head>
+	</head>
+	<body>
+		<p>
+			This message prints out submitted php info.
+			<?php echo $message ?>
+		</p>
+	</body>
+</html>
